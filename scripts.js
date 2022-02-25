ed313a3b4a2fbc8498a9bd1dc78b235bdead242e
berkay <berkayurun@outlook.com>
2022-02-25 21:37:28 +0300

    added a reset button

--- scripts.js
+++ scripts.js
@@ -1,20 +1,40 @@
 let playerScore = 0;
 let computerScore = 0;
 const result = document.querySelector(".result-display");
+const rematchButton = document.querySelector("button.hidden");
 const buttons = document.querySelectorAll(".options");
+const buttonsContainer = document.querySelector(".btn-container");
 buttons.forEach((button) =>
   button.addEventListener("click", () => {
     result.innerText = playRound(button.textContent, computerPlay());
     checkWinner();
     displayResults();
+
+    if (checkWinner()) {
+      buttons.forEach((button) => button.classList.add("hidden"));
+      rematchButton.classList.remove("hidden");
+      rematchButton.addEventListener("click", () => {
+        playerScore = 0;
+        computerScore = 0;
+        displayResults();
+        rematchButton.classList.add("hidden");
+        buttons.forEach((button) => button.classList.remove("hidden"));
+      });
+
+      buttonsContainer.appendChild(rematchButton);
+    }
   })
 );
 
 function checkWinner() {
   if (playerScore === 5) {
     result.innerText = `Congrats! Player has won!`;
+    return true;
   } else if (computerScore === 5) {
     result.innerText = `What a shame! Computer has won!`;
+    return true;
+  } else {
+    return false;
   }
 }
 
